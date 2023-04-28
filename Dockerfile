da12ef79f1c01d478548c59a58d9f8f2b9cde139
rictuar <parkerbremer@gmail.com>
2023-04-28 16:54:59 -0700

    updates

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,15 @@ FROM continuumio/miniconda3
 
 COPY ./sample_ingester_api_min.yml /
 
-RUN conda env create -f sample_ingester_api_min.yml 
+
 
 RUN mkdir /pages && mkdir /assets && mkdir /additional_files
 
 COPY ./additional_files/* /additional_files/
 
 COPY ./*.py ./
+
+RUN conda env create -f sample_ingester_api_min.yml 
 
 WORKDIR ./
 
--- newvocabularyuploadchecker.py
+++ newvocabularyuploadchecker.py
@@ -8,13 +8,13 @@ class NewVocabularyUploadChecker:
         self.error_list=list()
         
     def check_char_length(self):
-        print(self.vocabulary_list)
+        # print(self.vocabulary_list)
         for new_vocab_term in self.vocabulary_list:
-            print(new_vocab_term)
+            # print(new_vocab_term)
             if new_vocab_term is None:
                 continue
             if len(new_vocab_term)<3:
-                print('<3')
+                # print('<3')
                 self.error_list.append(
                     f'{new_vocab_term} must contain at least 3 characters'
                 )
